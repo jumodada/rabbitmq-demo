5585f89a885b7e65b0bcb0a3aa329f811d1c25db
xipper <864353360@qq.com>
2022-12-16 13:36:36 +0800

    simple mode

--- Rabbitmq/rabbitmq.go
+++ Rabbitmq/rabbitmq.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/streadway/amqp"
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+	"log"
+)
 
 const MQUrl = "amqp://admin:123@127.0.0.1:5672/test"
 
@@ -13,10 +17,56 @@ type RabbitMQ struct {
 	MqUrl    string
 }
 
-func newRabbit(queueName string, exchange string) *RabbitMQ {
-	return &RabbitMQ{QueName: queueName, Exchange: exchange}
+func newRabbit(queueName string, exchange string, key string) *RabbitMQ {
+	return &RabbitMQ{QueName: queueName, Exchange: exchange, Key: key}
 }
 
-func main() {
+func (r *RabbitMQ) Destroy() {
+	r.channel.Close()
+	r.conn.Close()
+}
+
+func (r *RabbitMQ) failOnError(err error, message string) {
+	if err != nil {
+		log.Fatalf("%s:%s", message, err)
+		panic(fmt.Sprintf("%s:%s", message, err))
+	}
+}
+
+func rabbitSimpleMode(queueName string) *RabbitMQ {
+	rabbitmq := newRabbit(queueName, "", "")
+	var err error
+	rabbitmq.conn, err = amqp.Dial(rabbitmq.MqUrl)
+	rabbitmq.failOnError(err, "connected error")
+	rabbitmq.channel, err = rabbitmq.conn.Channel()
+	rabbitmq.failOnError(err, "getting channel error")
+	return rabbitmq
+}
+
+func (r *RabbitMQ) PublishSimple(message string) {
+	_, err := r.channel.QueueDeclare(r.QueName,
+		// 是否持久化
+		false,
+		//是否自动删除
+		false,
+		// 是否具有排他性
+		false,
+		//是否阻塞
+		false,
+		nil,
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+	r.channel.Publish(
+		r.Exchange,
+		r.QueName,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		},
+	)
 
 }
